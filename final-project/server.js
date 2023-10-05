99b718bb8bd51fafafd270d750850dd0f55901e2
Ali Raza <aliraza.ar7785@gmail.com>
2023-10-06 00:32:21 +0500

    implementing ejs

---
+++ final-project/server.js
@@ -0,0 +1,24 @@
+var expressLayouts = require("express-ejs-layouts");
+let express = require("express");
+
+let server = express();
+server.use(express.static("public"));
+server.set("view engine", "ejs");
+
+server.use(expressLayouts);
+
+server.get("/", function (req, res) {
+  res.render("landing");
+});
+
+server.get("/login", function (req, res) {
+  res.render("login");
+});
+
+server.get("/register", function (req, res) {
+  res.render("register");
+});
+
+server.listen(5000, function () {
+  console.log("server is running at Port 5000");
+});
